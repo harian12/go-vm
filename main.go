d482530008f541e54185ea09c535033b83b2801d
harian12 <hariansyah434@gmail.com>
2022-07-14 14:58:20 +0700

    update main 2

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"status":  true,
-			"message": "update app baru dicoba 1",
+			"message": "update app baru dicoba 2",
 			"data":    []string{},
 		})
 	})
